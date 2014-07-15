1441c5a79ba828f5ff09790169d76a5e684ed3bb
iamalarner <matt.larner.dev@gmail.com>
2014-07-15 13:11:53 +1000

    update example

--- example/index.js
+++ example/index.js
@@ -2,50 +2,20 @@ var host = '127.0.0.1',
     port = '6379',
     subscribe = require('../')(port, host);
 
-// VARIABLE
+subscribe('thing*', function(error, emitter){
+    var count = 0;
 
-var subscription = subscribe('thing', function(error){
-    if(error) {
-        return console.log(error);
-    }
-});
-
-subscription.on('message', function(message, channel) {
-    console.log('MESSAGE: '+ message + ' CHANNEL X: ' + channel);
-
-    if(message === 'destroy the thing'){
-        this.unsubscribe();
-    }
-});
-
-// CHAINING
-
-subscribe('thing*', function(error){
-    if(error) {
-        return console.log(error);
-    }
-}).on('message', function(message, channel) {
-    console.log('MESSAGE: '+ message + ' CHANNEL Y: ' + channel);
-
-    if(message === 'destroy'){
-        subscription.unsubscribe();
-        this.unsubscribe();
-    }
-});
-
-// FUNCTION SCOPE
-
-subscribe('thingy', function(error, emitter){
     if(error) {
         return console.log(error);
     }
 
     emitter.on('message', function(message, channel) {
-        console.log('MESSAGE: '+ message + ' CHANNEL Z: ' + channel);
-        if(message === 'destroy'){
-            emitter.unsubscribe(function(error, confirmation) {
-                console.log(confirmation);
-            });
+
+        console.log('MESSAGE: '+ message + ' CHANNEL: ' + channel);
+
+        count++;
+        if(count >= 5) {
+            emitter.unsubscribe();
         }
     });
 });
